1348cc68ed4c9fc6230a33803848ab86b6a30c9c
Artem <165590633+artemkotko14@users.noreply.github.com>
2024-04-13 10:53:37 +0000

    another option

--- RandomPokemon.js
+++ RandomPokemon.js
@@ -1,15 +1,15 @@
 import "./RandomPokemon.css";
 export default function RandomPokemon() {
-    const baseUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
-    const random = Math.floor((Math.random() * 151) + 1)
-    return(
-        <div className="RandomPokemon">
-        <h1>Pokemon# {random}</h1>
-        <img
-        src={ baseUrl + random + '.png'}
-        alt={'Pokemon# + random'}
-      />  
-      </div>
-    ) 
-  }
-  
+  // const random = Math.floor((Math.random() * 151) + 1)
+  // const baseUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${random}.png`;
+  //  src={ baseUrl }
+  const baseUrl =
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
+  const random = Math.floor(Math.random() * 151 + 1);
+  return (
+    <div className="RandomPokemon">
+      <h1>Pokemon# {random}</h1>
+      <img src={baseUrl + random + ".png"} alt={"Pokemon# + random"} />
+    </div>
+  );
+}
